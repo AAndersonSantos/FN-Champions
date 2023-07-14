45a49edaa6e70587da84a95cca19ac2f04a9621e
Anderson Santos <anderson.santos@fngroup.com.br>
2023-07-14 11:11:53 -0300

    edit page

--- main.js
+++ main.js
@@ -16,3 +16,19 @@ $('#carousel-1').owlCarousel({
         }
     }
 })
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 30;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } 
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
